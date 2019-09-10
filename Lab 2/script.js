5f33d49a06349e7207cbd95d13f90b5b0b431720
Giovanni <g.l.mierop@gmail.com>
2019-09-10 15:17:50 +0200

    Lab 2 aangepast. Lab 3 bijna af.

--- Lab 2/script.js
+++ Lab 2/script.js
@@ -1,8 +1,8 @@
 // Giovanni Mierop
 // Da Vinci College - Applicatie Ontwikkelaar
 
-
-alert("U dient 3 vragen te antwoorden.");
+const ALERT_MSG = "Je dient 3 vragen te antwoorden."
+alert(ALERT_MSG);
 
 // maakt een prompt aan en vraagt naar je voornaam, achternaam en leeftijd. Wat je invoerd wordt in het geheugen opgeslagen.
 var naam = prompt("Wat is je naam?", "Voornaam");
@@ -14,6 +14,6 @@ var leeftijd = prompt("Hoe oud ben je?", "18");
 // Hier worden de variables zichtbaar gemaakt op het document.
 document.write("Voornaam: " + naam + "<br>");
 
-document.write("achternaam: " + achternaam + "<br>");
+document.write("Achternaam: " + achternaam + "<br>");
 
 document.write("Leeftijd: " + leeftijd);
---
+++ Lab 3/tafel van 6.js
@@ -0,0 +1,5 @@
+document.write("<h2>Tafel van 6</h2>");
+for (var i = 0; i < 11; i++) {
+    var result = i * 6;
+    document.write(i + " x 6 = " + result + "<br>");
+}
