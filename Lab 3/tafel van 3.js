305150b2e15821e9118ed69452ad05734ad80603
Giovanni <g.l.mierop@gmail.com>
2019-09-10 20:30:42 +0200

    Pizza calculator

--- Lab 3/tafel van 3.js
+++ Lab 3/tafel van 3.js
@@ -1,4 +1,3 @@
-document.write(prompt("hello there"));
 document.write("<h2>Tafel van 3</h2>")
 for (var i = 0; i < 11; i++) {
     var result = i * 3;
---
+++ Pizza calculator/script.js
@@ -0,0 +1,4 @@
+const SIZE_SMALL = 5.99;
+const SIZE_MEDIUM = 7.99;
+const SIZE_LARGE = 12.99;
+
