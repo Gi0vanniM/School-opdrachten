8ee1cb7af8c567e1dc12cc902545d10f19a404da
Giovanni <g.l.mierop@gmail.com>
2019-11-20 14:35:16 +0100

    yoda

--- Blok 2/Week 11 tm 13/adventuregame-template/adventure.js
+++ Blok 2/Week 11 tm 13/adventuregame-template/adventure.js
@@ -1,2 +1,17 @@
+
 var music = new Audio("Assets/Sound/magus.mp3");
 music.play();
+
+document.head += '<link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">'
+document.getElementById("title").innerHTML = "Doing groceries at Qualbert Ceyn";
+document.getElementById("description").innerHTML = "Welcome to <b>The Game</b><br>Press Start";
+document.getElementById("button1").innerHTML = "-"
+document.getElementById("button2").innerHTML = "START"
+document.getElementById("button3").innerHTML = "-"
+
+document.getElementById("button2").addEventListener("click", function yo() { start(); });
+
+function start() {
+    music.pause();
+
+}
