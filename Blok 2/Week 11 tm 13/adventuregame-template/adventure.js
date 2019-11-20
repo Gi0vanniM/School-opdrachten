3a94f82d82cb060c20d1617f3b2e60e91bc3a53a
Giovanni <g.l.mierop@gmail.com>
2019-11-20 14:35:16 +0100

    yoda

--- Blok 2/Week 11 tm 13/adventuregame-template/adventure.js
+++ Blok 2/Week 11 tm 13/adventuregame-template/adventure.js
@@ -1,2 +1,8 @@
-var music = new Audio("Assets/Sound/magus.mp3");
-music.play();
+import { module } from './setup';
+
+
+document.getElementById("button2").addEventListener("click", function yo() { alert("yoda"); });
+
+function start() {
+
+}
