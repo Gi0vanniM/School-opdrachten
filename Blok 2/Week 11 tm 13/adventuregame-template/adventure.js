bb6cf276d71054118dbb5ecd6aaeea2526c7a791
Giovanni <g.l.mierop@gmail.com>
2019-11-22 20:29:11 +0100

    Commit for today

--- Blok 2/Week 11 tm 13/adventuregame-template/adventure.js
+++ Blok 2/Week 11 tm 13/adventuregame-template/adventure.js
@@ -1,17 +1,40 @@
 
+var started = false;
 var music = new Audio("Assets/Sound/magus.mp3");
+
+var title = document.getElementById("title");
+var desc = document.getElementById("description");
+var button1 = document.getElementById("button1");
+var button2 = document.getElementById("button2");
+var button3 = document.getElementById("button3");
+var gameButtons = document.getElementById("game-buttons");
+
+var img = document.createElement("img");
+img.src = "Assets/Images/press_start.jpg";
+img.id = "imaaag"
+// document.getElementById("game-container").appendChild(img);
+gameButtons.before(img);
+
 music.play();
 
 document.head += '<link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">'
-document.getElementById("title").innerHTML = "Doing groceries at Qualbert Ceyn";
-document.getElementById("description").innerHTML = "Welcome to <b>The Game</b><br>Press Start";
-document.getElementById("button1").innerHTML = "-"
-document.getElementById("button2").innerHTML = "START"
-document.getElementById("button3").innerHTML = "-"
+title.innerHTML = "Doing groceries at Qualbert Ceyn";
+desc.innerHTML = "Welcome to <b>The Game</b>";
+button1.innerHTML = "-"
+button2.innerHTML = "START"
+button3.innerHTML = "-"
 
-document.getElementById("button2").addEventListener("click", function yo() { start(); });
+button2.setAttribute("onClick", "start();");
 
 function start() {
-    music.pause();
+    if (started == false) {
+        started = true;
+        music.pause();
+        alert("Starting...");
 
+        button1.innerHTML = "A";
+        button2.innerHTML = "B";
+        button3.innerHTML = "C";
+
+    }
 }
