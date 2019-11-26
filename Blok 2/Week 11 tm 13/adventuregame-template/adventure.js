e78b9c59bb18223d05497c7163d9a2d027a7703f
Giovanni <g.l.mierop@gmail.com>
2019-11-26 21:51:20 +0100

    changed inventory look

--- Blok 2/Week 11 tm 13/adventuregame-template/adventure.js
+++ Blok 2/Week 11 tm 13/adventuregame-template/adventure.js
@@ -2,6 +2,7 @@
 var started = false;
 var music = new Audio("Assets/Sound/magus.mp3");
 
+var scene = 0;
 var title = document.getElementById("title");
 var desc = document.getElementById("description");
 var button1 = document.getElementById("button1");
@@ -9,7 +10,7 @@ var button2 = document.getElementById("button2");
 var button3 = document.getElementById("button3");
 var gameButtons = document.getElementById("game-buttons");
 var inventoryItem = document.getElementById("inventoryItem");
-inventoryItem.alt = "Inventory\n";
+inventoryItem.alt = "Inventory";
 
 var inventory = [];
 
@@ -17,8 +18,9 @@ var inventoryDisplayItems = [
     { name: "clothes", location: "Assets/Images/clothes.png", id: "inv_clothes" },
     { name: "phone", location: "Assets/Images/phone.png", id: "inv_phone" },
     { name: "keys", location: "Assets/Images/keys.png", id: "inv_keys" },
-    { name: "wallet", location: "Assets/Images/wallet.png", id: "inv_wallet" }
-]
+    { name: "wallet", location: "Assets/Images/wallet.png", id: "inv_wallet" },
+    { name: "gun", location: "Assets/Images/gun.png", id: "inv_gun" }
+];
 
 var display = document.createElement("div");
 display.id = "display";
@@ -32,6 +34,12 @@ document.getElementById("display").appendChild(img);
 var inventoryDisplay = document.createElement("div");
 inventoryDisplay.id = "invDisplay"
 document.getElementById("game-container").appendChild(inventoryDisplay);
+inventoryDisplay.innerHTML = "empty"
+
+var curruntScene = document.createElement("div");
+curruntScene.id = "currentScene";
+document.body.appendChild(curruntScene);
+curruntScene.innerHTML = "start_screen";
 
 music.play();
 
@@ -42,6 +50,7 @@ button1.innerHTML = "-"
 button2.innerHTML = "START"
 button3.innerHTML = "-"
 
+button1.setAttribute("onClick", "music.play();");
 button2.setAttribute("onClick", "start();");
 
 function start() {
@@ -59,12 +68,17 @@ function start() {
 }
 
 function scene0_WakingUp0() {
+    console.log("start scene0_WakingUp0");
+    curruntScene.innerHTML = "scene0_WakingUp0";
+    scene++;
+
     img.src = "Assets/Images/bedroom.jpg"
     desc.innerHTML = "You wake up in your bedroom. " +
         "You see a note saying you need to go grocery shopping. " +
         "With it a list of what you need to buy.<br><br>" +
-        "First you need to grab your stuff before you can go.<br><br>" +
-        "<ul id='get_stuff'><b>A</b>: Clothes<br>" +
+        //
+        "First you need to grab your stuff before you can go.<br>" +
+        "<ul id='actions'><b>A</b>: Clothes<br>" +
         "<b>B</b>: Keys and wallet<br>" +
         "<b>C</b>: Phone</ul>";
 
@@ -74,15 +88,48 @@ function scene0_WakingUp0() {
     loadObject("clothes");
     loadObject("phone");
     loadObject("walletAndKeys");
+    loadObject("gun");
 
     button1.setAttribute("onClick", "pickUpItem('clothes');");
     button2.setAttribute("onClick", "pickUpItem('phone');");
     button3.setAttribute("onClick", "pickUpItem('walletAndKeys');");
+}
+
+function scene0_WakingUp1() {
+    console.log("start scene0_WakingUp1");
+    curruntScene.innerHTML = "scene0_WakingUp1";
+    scene++;
+
+    img.src = "Assets/Images/bedroom.jpg"
+    desc.innerHTML = "You put on your clothes. " +
+        "You now need to go outside" +
+        "With it a list of what you need to buy.<br><br>" +
+        //
+        "Actions:<br>" +
+        "<ul id='actions'><b>A</b>: Leave bedroom<br>" +
+        "<b>B</b>: <br>" +
+        "<b>C</b>: Take gun</ul>";
+
+    button1.setAttribute("onClick", "scene1_LivingRoom0();");
+    button2.setAttribute("onClick", "");
+    button3.setAttribute("onClick", "pickUpItem('gun');");
 
 }
 
+function scene1_LivingRoom0() {
+    console.log("start scene1_LivingRoom0");
+    curruntScene.innerHTML = "scene1_LivingRoom0";
+    scene++;
+
+    img.src = "Assets/Images/livingroom.jpg"
+    desc.innerHTML = "lr";
+}
+
+//#######################################
+
 function pickUpItem(item) {
-    if (item == "clothes" || item == "phone" || item == "walletAndKeys") {
+    if (item == "clothes" || item == "phone"
+        || item == "walletAndKeys" || item == "gun") {
         if (!inventory.includes(item)) {
 
             document.getElementById("img_" + item).style.visibility = "hidden";
@@ -96,12 +143,17 @@ function pickUpItem(item) {
             }
             updateInventory();
         }
+        if (inventory.includes("clothes") && inventory.includes("phone")
+            && inventory.includes("keys") && inventory.includes("wallet")
+            && scene == 1) {
+            scene0_WakingUp1();
+        }
     }
 }
 
 function loadObject(objectName) {
 
-    if (objectName == "clothes" || objectName == "phone" || objectName == "walletAndKeys") {
+    if (objectName == "clothes" || objectName == "phone" || objectName == "walletAndKeys" || objectName == "gun") {
         if (!inventory.includes(objectName)) {
             var imgObject = document.createElement("img");
             imgObject.src = "Assets/Images/" + objectName + ".png";
@@ -109,22 +161,6 @@ function loadObject(objectName) {
             document.getElementById("display").appendChild(imgObject);
         }
     }
-    // if (objectName == "phone") {
-    //     if (!inventory.includes("clothes")) {
-    //         var imgPhone = document.createElement("img");
-    //         imgPhone.src = "Assets/Images/phone.png";
-    //         imgPhone.id = "img_phone";
-    //         document.getElementById("display").appendChild(imgPhone);
-    //     }
-    // }
-    // if (objectName == "walletAndKeys") {
-    //     if (!inventory.includes("wallet") && !inventory.includes("keys")) {
-    //         var imgWalletAndKeys = document.createElement("img");
-    //         imgWalletAndKeys.src = "Assets/Images/walletAndKeys.png";
-    //         imgWalletAndKeys.id = "img_walletandkeys";
-    //         document.getElementById("display").appendChild(imgWalletAndKeys);
-    //     }
-    // }
 }
 
 function updateInventory() {
@@ -137,6 +173,7 @@ function updateInventory() {
         var imag = document.createElement("img");
         imag.src = imgLoc.location
         imag.id = imgLoc.id;
+        imag.title = inventory[i];
         imag.className = "invItem";
         inventoryDisplay.appendChild(imag);
     }
