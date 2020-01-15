096c8a82b7800c94792ac304d92739ea3196dbbd
Giovanni <g.l.mierop@gmail.com>
2020-01-15 23:41:27 +0100

    .

--- Blok 2/Week 11 tm 13/lingo/script.js
+++ Blok 2/Week 11 tm 13/lingo/script.js
@@ -40,21 +40,35 @@ function check() {
                 // veranderd letter naar groen
                 document.getElementById(id).className += " green";
                 // veranderd letter naar een streepje zodat deze straks niet dubbel gechekt wordt
-                tempAnswer[i] = "-";
+                tempAnswer[i] = "-"; 
+                
             }
         }
 
+        // letters die geel moeten zijn
         for (var i = 0; i < 5; i++) {
+            // kijkt of tempAnswer de i'ste letter bevat
             if (tempAnswer.includes(input[i])) {
+                // kijkt of dat de letter die hij gevonden heeft niet al een keer gepakt is
+                if (tempAnswer[i] != "-") {
+                    //
                 var id = attempts + "-" + i;
                 document.getElementById(id).className += " yellow";
-                tempAnswer[i] = "-";
+                //
+                // pakt de positie van de (input[i]) letter
+                // en vervangt de letter met een streepje
+                var a = tempAnswer.indexOf(input[i]);
+                tempAnswer[a] = "-";
             }
+        }
             else if (tempAnswer[i] != "-") {
                 var id = attempts + "-" + i;
                 document.getElementById(id).className = "letter";
+                // console.log(i + " zit er niet in, " + tempAnswer);
             }
         }
+        console.log("tempAnswer after: "+ tempAnswer);
+        
         writeLetter(input);
         if (attempts == 4) {
             alert("Helaas je hebt het word niet geraden.")
@@ -65,6 +79,56 @@ function check() {
         alert("Input is te klein of te groot. Het moet altijd 5 letters zijn.")
     }
 }
+
+// function check() {
+//     input = Array.from(document.getElementById("input-box").value.toUpperCase());
+
+//     // maakt een temporary varuable aan
+//     var tempAnswer = [answer[0], answer[1], answer[2], answer[3], answer[4]];
+
+//     if (input.length == 5) {
+
+//         if (input.toString() == answer.toString()) {
+//             alert("Wow je hebt het geraden! Episch!");
+//         }
+
+//         for (var i = 0; i < 5; i++) {
+//             // checkt letter voor letter of een letter op de goede plaats staat
+//             if (tempAnswer[i] == input[i]) {
+//                 // ID div
+//                 var id = attempts + "-" + i;
+//                 // veranderd letter naar groen
+//                 document.getElementById(id).className += " green";
+//                 // veranderd letter naar een streepje zodat deze straks niet dubbel gechekt wordt
+//                 tempAnswer[i] = "-";
+//                 console.log(i + " is goed, " + tempAnswer);
+                
+//             }
+//         }
+
+//         for (var i = 0; i < 5; i++) {
+//             if (tempAnswer.includes(input[i])) {
+//                 var id = attempts + "-" + i;
+//                 document.getElementById(id).className += " yellow";
+//                 tempAnswer[i] = "-";
+//                 console.log(i + " zit er in, " + tempAnswer);
+//             }
+//             else if (tempAnswer[i] != "-") {
+//                 var id = attempts + "-" + i;
+//                 document.getElementById(id).className = "letter";
+//                 console.log(i + " zit er niet in, " + tempAnswer);
+//             }
+//         }
+//         writeLetter(input);
+//         if (attempts == 4) {
+//             alert("Helaas je hebt het word niet geraden.")
+//         }
+//         attempts++;
+
+//     } else {
+//         alert("Input is te klein of te groot. Het moet altijd 5 letters zijn.")
+//     }
+// }
 
 function writeLetter(putin) {
     for (var i = 0; i < 5; i++) {
